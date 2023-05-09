2d5d35610ccdafeece90baae996b571d1c1550ca
Rafael Martínez <48167635+Rafa-77@users.noreply.github.com>
2023-05-08 20:26:57 -0600

    Add files via upload

---
+++ Using Databases with Python/5 - Databases and Visualization (peer-graded).py
@@ -0,0 +1,25 @@
+# Retrieving GEOData
+
+# Download the code from http://www.py4e.com/code3/geodata.zip -
+# then unzip the file and edit where.data to add an address nearby where you live - don't reveal where you live.
+# Then run the geoload.py to lookup all of the entries in where.data (including the new one) and produce the geodata.sqlite.
+# Then run geodump.py to read the database and produce where.js.
+# You can run the programs and then scroll back to take your screen shots when the program finishes.
+# Then open where.html to visualize the map. Take screen shots as described below.
+# Make sure that your added location shows in all three of your screen shots.
+
+# Screen shot (JPG or PNG - Maximum 1MB) of the geoload.py program output
+# in the command line including your highlighted added location.
+# (Please use PNG or JPG files 1024KB max)
+
+
+# Screen shot (JPG or PNG - Maximum 1MB) of the geodump.py program output
+# in the command line including your highlighted added location.
+# (Please use PNG or JPG files 1024KB max)
+
+
+# Screen shot (JPG or PNG - Maximum 1MB) of the map zoomed into the location that you added
+# after you open where.html in a browser
+# (Please use PNG or JPG files 1024KB max)
+
+# d:/alex_/Documents/Programacion/venv/Scripts/python.exe "d:/alex_/Documents/Programacion/Cursos/Python for Everybody/code3/geodata/geoload.py"
