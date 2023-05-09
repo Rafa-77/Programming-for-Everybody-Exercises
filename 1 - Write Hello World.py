41063955cc08ea4929d9d2997f8c3f6df8296736
Rafael Martínez <48167635+Rafa-77@users.noreply.github.com>
2023-05-08 19:59:30 -0600

    Add files via upload

---
+++ 1 - Write Hello World.py
@@ -0,0 +1,9 @@
+# Missing Assignments Correspond To Quizzes.
+
+
+# Assignment: Write Hello World
+
+# Write a program that uses a print statement to say 'hello world' as shown in 'Desired Output'.
+# Desired Output: hello world
+
+print("hello world")
