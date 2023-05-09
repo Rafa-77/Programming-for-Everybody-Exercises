d3d6bb63d7c36f66b3ddcb945c7912cabb1d5cb9
Rafael Martínez <48167635+Rafa-77@users.noreply.github.com>
2023-05-08 20:18:19 -0600

    Add files via upload

---
+++ 1 - Our First Database.py
@@ -0,0 +1,24 @@
+# Create a SQLITE database or use an existing database and create a table in the database called "Ages":
+
+# CREATE TABLE Ages (
+#   name VARCHAR(128),
+#   age INTEGER
+# )
+
+# Then make sure the table is empty by deleting any rows that you previously inserted, and insert these rows
+# and only these rows with the following commands:
+
+# DELETE FROM Ages;
+# INSERT INTO Ages (name, age) VALUES ('Jonah', 39);
+# INSERT INTO Ages (name, age) VALUES ('Eiddon', 31);
+# INSERT INTO Ages (name, age) VALUES ('Stanley', 39);
+# INSERT INTO Ages (name, age) VALUES ('Chelsi', 17);
+# INSERT INTO Ages (name, age) VALUES ('Iana', 22);
+
+# Once the inserts are done, run the following SQL command:
+# SELECT hex(name || age) AS X FROM Ages ORDER BY X
+
+# Find the first row in the resulting record set and enter the long string that looks like 53656C696E613333.
+
+
+# LO HICE EN SQLITE, copie y pegue lo indicado
